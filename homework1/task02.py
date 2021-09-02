705c7aa86a8bd11f3c033aed9cf6d0f5f5430fa8
Yulia Penkovskaya <7930789@gmail.com>
2021-09-02 22:41:56 +0300

    fix flake8

--- homework1/task02.py
+++ homework1/task02.py
@@ -8,9 +8,12 @@ def check_fibonacci(seq: Sequence[int]) -> bool:
     if len(seq) == 1:
         return is_num_in_fibonacci(seq[0])
     if len(seq) == 2:
-        if (seq[0] >= seq[1] and seq[1] is not 0) and (is_num_in_fibonacci(seq[0]) and is_num_in_fibonacci(seq[1])):
+        if (
+            (seq[0] >= seq[1] != 0)
+            and (is_num_in_fibonacci(seq[0]) and is_num_in_fibonacci(seq[1]))
+        ):
             return is_num_in_fibonacci(seq[0] + seq[1])
-    if len(seq) > 2 and seq[1] is not 0:
+    if len(seq) > 2 and seq[1] != 0:
         mid_check = []
         for i in range(2, len(seq)):
             mid_check.append((seq[i] - seq[i - 1] == seq[i - 2]))
@@ -26,4 +29,5 @@ def is_num_in_fibonacci(num: int) -> bool:
     The formula has been taken from the nice Wikipedia page:
     https://ru.wikipedia.org/wiki/Числа_Фибоначчи#Свойства
     """
-    return ((5*(num**2) + 4)**0.5).is_integer() or ((5*(num**2) - 4)**0.5).is_integer()
+    return ((5 * (num ** 2) + 4) ** 0.5).is_integer() \
+        or ((5 * (num ** 2) - 4) ** 0.5).is_integer()
--- homework1/task04.py
+++ homework1/task04.py
@@ -4,5 +4,8 @@ import itertools
 
 
 def check_sum_of_any(*args: Tuple[List[int]]) -> int:
-    """Takes integer from each list, computes sums, and returns how many sums are equal to zero"""
+    """
+    Takes integer from each list, computes sums,
+    and returns how many sums are equal to zero
+    """
     return sum(sum(i) == 0 for i in itertools.product(*args))
