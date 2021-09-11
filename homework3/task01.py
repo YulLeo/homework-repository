8b0ac2b4d5a6e09a3c8bf1f7014300e6ecdbc06b
Yulia Penkovskaya <7930789@gmail.com>
2021-09-11 16:49:29 +0300

    fix homework3 task01

--- homework3/task01.py
+++ homework3/task01.py
@@ -4,26 +4,25 @@ In previous homework task 4, you wrote a cache function
 that remembers other function output value.
 Modify it to be a parametrized decorator.
 """
-
 from collections import defaultdict
 from typing import Callable
 
 
 def decorator_cache(times: int) -> Callable:
-    """
-    Decorator that caches function return
-    values up to times number only
-    """
     def cache(func: Callable) -> Callable:
-        def wrapper(*args):
-            if args in func_cache and (
-                    args not in func_times or func_times[args] < times):
-                func_times[args] += 1
-                return func_cache[args]
-            if args in func_cache and func_times[args] == times-1:
-                func_times[args] = 0
-                return func_cache.pop(args)
-            res = func_cache[args] = func(*args)
+        """Decorator that caches function return values"""
+        def wrapper(*args, **kwargs):
+            saved_args = str(sorted(args))+str(sorted(kwargs.items()))
+            if saved_args in func_cache and (
+                    saved_args not in func_times
+                    or func_times[saved_args] < times):
+                func_times[saved_args] += 1
+                return func_cache[saved_args]
+            if saved_args in func_cache and (
+                    func_times[saved_args] == times - 1):
+                func_times[saved_args] = 0
+                return func_cache.pop(saved_args)
+            res = func_cache[saved_args] = func(*args, **kwargs)
             return res
         func_cache = {}
         func_times = defaultdict(int)
--- tests/homework3/test_task01.py
+++ tests/homework3/test_task01.py
@@ -23,3 +23,31 @@ def test_no_cash():
     val_1 = func(*some)
     val_2 = func(*some)
     assert val_1 is not val_2
+
+
+def test_cache_args_and_kwargs():
+    @decorator_cache(times=2)
+    def func(a, b, **kwargs):
+        return a, b, kwargs
+
+    val_1 = func(100, 200, kwarg1='spam')
+    val_2 = func(100, 200, kwarg1='spam')
+    val_3 = func(100, 200, kwarg1='spam')
+    val_4 = func(100, 200, kwarg1='spam')
+    assert val_1 is val_2
+    assert val_2 is val_3
+    assert val_3 is not val_4
+
+
+def test_cache_swap_kwargs():
+    @decorator_cache(times=2)
+    def func(**kwargs):
+        return kwargs
+
+    val_1 = func(kwarg1='spam', kwarg2='ham')
+    val_2 = func(kwarg2='ham', kwarg1='spam')
+    val_3 = func(kwarg2='ham', kwarg1='spam')
+    val_4 = func(kwarg2='ham', kwarg1='spam')
+    assert val_1 is val_2
+    assert val_2 is val_3
+    assert val_3 is not val_4
