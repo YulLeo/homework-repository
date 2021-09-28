1282203830d8c277c57809fcf984bfc3d8f96b6e
Yulia Penkovskaya <7930789@gmail.com>
2021-09-28 20:39:33 +0300

    fix homework6 test_task02

--- tests/homework6/test_task02.py
+++ tests/homework6/test_task02.py
@@ -5,6 +5,12 @@ import pytest
 from homework6.exceptions import DeadlineError
 from homework6.task02 import HomeworkResult, Student, Teacher
 
+LATE = 'You are late'
+
+OBJECT_ERROR = 'You gave not a Homework object'
+
+HOMEWORK_SOLUTION = 'homework_solution'
+
 
 @pytest.fixture()
 def instances():
@@ -29,31 +35,31 @@ def test_names_students(instances):
 
 
 def test_value_error(instances):
-    with pytest.raises(ValueError, match='You gave not a Homework object'):
-        HomeworkResult(instances.opp_teacher, 'fgfgfh', instances.good_student)
+    with pytest.raises(ValueError, match=OBJECT_ERROR):
+        HomeworkResult(instances.opp_teacher, HOMEWORK_SOLUTION, instances.good_student)
 
 
 def test_deadline(instances):
-    with pytest.raises(DeadlineError, match='You are late'):
-        instances.good_student.do_homework(instances.oop_hw, 'hgjhgjfhgj')
+    with pytest.raises(DeadlineError, match=LATE):
+        instances.good_student.do_homework(instances.oop_hw, HOMEWORK_SOLUTION)
 
 
 def test_check_homework_true(instances):
     result = instances.good_student.do_homework(
-        instances.docs_hw, 'I have done this hw')
+        instances.docs_hw, HOMEWORK_SOLUTION)
     assert instances.opp_teacher.check_homework(result)
 
 
 def test_homework_done_different_teachers(instances):
     result = instances.good_student.do_homework(
-        instances.docs_hw, 'I have done this hw')
+        instances.docs_hw, HOMEWORK_SOLUTION)
     instances.opp_teacher.check_homework(result)
     assert instances.advanced_python_teacher.homework_done[instances.docs_hw]
 
 
 def test_homework_done_doubles(instances):
     result = instances.good_student.do_homework(
-        instances.docs_hw, 'I have done this hw')
+        instances.docs_hw, HOMEWORK_SOLUTION)
     instances.opp_teacher.check_homework(result)
     instances.opp_teacher.check_homework(result)
     assert len(instances.opp_teacher.homework_done[instances.docs_hw]) == 1
