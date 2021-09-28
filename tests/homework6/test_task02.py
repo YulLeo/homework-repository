72429b1f4e8d452479b2487d837288b028042536
Yulia Penkovskaya <7930789@gmail.com>
2021-09-28 20:56:22 +0300

    fix homework6 test_task02

--- tests/homework6/test_task02.py
+++ tests/homework6/test_task02.py
@@ -9,57 +9,52 @@ LATE = 'You are late'
 
 OBJECT_ERROR = 'You gave not a Homework object'
 
-HOMEWORK_SOLUTION = 'homework_solution'
+SOLUTION = 'homework_solution'
 
 
 @pytest.fixture()
 def instances():
     return SimpleNamespace(
-        advanced_python_teacher=Teacher('Aleksandr', 'Smetanin'),
-        opp_teacher=Teacher('Daniil', 'Shadrin'),
-        lazy_student=Student('Roman', 'Petrov'),
-        good_student=Student('Lev', 'Sokolov'),
+        teacher1=Teacher('Aleksandr', 'Smetanin'),
+        teacher2=Teacher('Daniil', 'Shadrin'),
+        student1=Student('Roman', 'Petrov'),
         oop_hw=Teacher('Ivan', 'Petrov').create_homework('Learn OOP', 0),
         docs_hw=Teacher('Elena', 'Ivanova').create_homework('Read docs', 5)
     )
 
 
 def test_names_teachers(instances):
-    assert instances.opp_teacher.first_name == 'Daniil'
-    assert instances.advanced_python_teacher.last_name == 'Smetanin'
+    assert instances.teacher2.first_name == 'Daniil'
+    assert instances.teacher1.last_name == 'Smetanin'
 
 
 def test_names_students(instances):
-    assert instances.lazy_student.last_name == 'Petrov'
-    assert instances.good_student.first_name == 'Lev'
+    assert instances.student1.last_name == 'Petrov'
 
 
 def test_value_error(instances):
     with pytest.raises(ValueError, match=OBJECT_ERROR):
-        HomeworkResult(instances.opp_teacher, HOMEWORK_SOLUTION, instances.good_student)
+        HomeworkResult(instances.teacher1, SOLUTION, instances.student1)
 
 
 def test_deadline(instances):
     with pytest.raises(DeadlineError, match=LATE):
-        instances.good_student.do_homework(instances.oop_hw, HOMEWORK_SOLUTION)
+        instances.student1.do_homework(instances.oop_hw, SOLUTION)
 
 
 def test_check_homework_true(instances):
-    result = instances.good_student.do_homework(
-        instances.docs_hw, HOMEWORK_SOLUTION)
-    assert instances.opp_teacher.check_homework(result)
+    result = instances.student1.do_homework(instances.docs_hw, SOLUTION)
+    assert instances.teacher2.check_homework(result)
 
 
 def test_homework_done_different_teachers(instances):
-    result = instances.good_student.do_homework(
-        instances.docs_hw, HOMEWORK_SOLUTION)
-    instances.opp_teacher.check_homework(result)
-    assert instances.advanced_python_teacher.homework_done[instances.docs_hw]
+    result = instances.student1.do_homework(instances.docs_hw, SOLUTION)
+    instances.teacher2.check_homework(result)
+    assert instances.teacher1.homework_done[instances.docs_hw]
 
 
 def test_homework_done_doubles(instances):
-    result = instances.good_student.do_homework(
-        instances.docs_hw, HOMEWORK_SOLUTION)
-    instances.opp_teacher.check_homework(result)
-    instances.opp_teacher.check_homework(result)
-    assert len(instances.opp_teacher.homework_done[instances.docs_hw]) == 1
+    result = instances.student1.do_homework(instances.docs_hw, SOLUTION)
+    instances.teacher2.check_homework(result)
+    instances.teacher2.check_homework(result)
+    assert len(instances.teacher2.homework_done[instances.docs_hw]) == 1
