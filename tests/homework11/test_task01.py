0d72d72e50e1a9eca423a0095cb7a34bf6e8df90
Yulia Penkovskaya <7930789@gmail.com>
2021-10-28 21:16:04 +0300

    add test for task01

--- tests/homework11/test_task01.py
+++ tests/homework11/test_task01.py
@@ -2,11 +2,11 @@ from homework11.task01 import SimplifiedEnum
 
 
 class ColorsEnum(metaclass=SimplifiedEnum):
-    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+    __keys = ('RED', 'RED', 'BLUE', 'ORANGE', 'BLACK')
 
 
 class SizesEnum(metaclass=SimplifiedEnum):
-    __keys = ("XL", "L", "M", "S", "XS")
+    __keys = ('XL', 'L', 'M', 'S', 'XS')
 
 
 def test_simplified_enum_colors():
@@ -15,3 +15,7 @@ def test_simplified_enum_colors():
 
 def test_simplified_enum_size():
     assert SizesEnum.M == 'M'
+
+
+def test_simplified_enum_colors_duplicates():
+    assert dir(ColorsEnum).count('RED') == 1
